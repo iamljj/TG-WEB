08137e879d012101c433bf19e77d3d9707229075
szk <2295559488@qq.com>
2021-11-18 09:53:29 +0800

    OA架构数据提供

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,8 @@ const dev = process.env.NODE_ENV === 'development'
 let REQUEST_ADDRESS = 'Online' //线上请求地址 baseurl
 let LOGIN_ADDRESS = 'Online'
 if (dev) {
-  ;(REQUEST_ADDRESS = 'http://192.168.0.48:9999'), //本地请求地址
-    (LOGIN_ADDRESS = 'http://192.168.0.48:9999') //登录请求地址
+  ;(REQUEST_ADDRESS = 'http://192.168.0.50:9999'), //本地请求地址
+    (LOGIN_ADDRESS = 'http://192.168.0.50:9999') //登录请求地址
 }
 // 传递给后端的token字段名
 const TOKEN_FIELD = 'token'
@@ -13,7 +13,6 @@ const CODE_VERIFICATION = true
 const LEGAL_CODES = {
   200: 'success',
   301: 'redirect',
-  304: 'redirect',
-  500: 'error'
+  304: 'redirect'
 }
 export { REQUEST_ADDRESS, TOKEN_FIELD, LEGAL_CODES, CODE_VERIFICATION, dev, LOGIN_ADDRESS }
